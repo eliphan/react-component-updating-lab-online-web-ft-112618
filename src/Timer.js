193eef500c018ee21b17df50d0d9ade46390e358
Elise Phan <aliceconnic@gmail.com>
2019-04-16 01:10:43 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -17,7 +17,7 @@ class Timer extends Component {
   
   componentDidUpdate() {
     this.timer.current.style.color =
-  "#" + Math.floor(Math.random() * 16777215).toString(16);
+         "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
   componentDidMount() {
